fa9ba7fc71a468b728230a2f00ca23159ad301ff
julianlosak <julianlosak@gmail.com>
2023-11-01 20:04:21 -0500

    updated quieries.js with new GetMe

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,18 +1,19 @@
 import gql from "graphql-tag";
 
 export const GET_ME = gql`
-  {
+  query GetMe {
     me {
       _id
       username
       email
+      bookCount
       savedBooks {
         bookId
         authors
         description
+        title
         image
         link
-        title
       }
     }
   }
