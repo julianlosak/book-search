b6fc1c91fff86c37a71346333e1f63daaaecf758
julianlosak <julianlosak@gmail.com>
2023-11-01 19:43:57 -0500

    adjusting middleware for graph QL API

--- server/utils/auth.js
+++ server/utils/auth.js
@@ -9,22 +9,31 @@ module.exports = {
     return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
   },
 
-  authMiddleware: function (req) {
-    let token = req.body.token || req.query.token || req.headers.authorization;
+  authMiddleware: function ({ req }) { // Destructure the req object
+    let token;
 
     if (req.headers.authorization) {
-      token = token.split(" ").pop().trim();
+      // Extract the token from the 'Authorization' header
+      token = req.headers.authorization.split(" ").pop().trim();
+    } else if (req.body.token) {
+      // Check if the token is in the request body
+      token = req.body.token;
+    } else if (req.query.token) {
+      // Check if the token is in the request query parameters
+      token = req.query.token;
     }
 
     if (!token) {
+      // If no token is found, return req as it is
       return req;
     }
 
     try {
       const { data } = jwt.verify(token, secret, { expiresIn: expiration });
+      // Attach the user data to the request object
       req.user = data;
-    } catch {
-      console.log("Invalid token");
+    } catch (error) {
+      console.log("Invalid token:", error.message); // Log the error message
     }
 
     return req;
